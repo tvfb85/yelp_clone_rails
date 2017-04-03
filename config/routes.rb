ad0936c76ced79e3b0c2b720f6baa2406d7111ff
Nicholas Leacock <nicholas@swissmail.com>
2017-04-03 22:47:16 +0100

    Added test suite to Gemfile, added first test.

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,14 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :restaurants
 
   # Example resource route with options:
   #   resources :products do
