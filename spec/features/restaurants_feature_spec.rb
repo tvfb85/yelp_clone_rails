105aa5a7d1b41c2775a9c4370c334d8d8134624e
Nicholas Leacock <nicholas@swissmail.com>
2017-04-04 14:04:55 +0100

    Added one more test, plus created Restaurant model

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -9,3 +9,15 @@ feature 'restaurants' do
     end
   end
 end
+
+context 'restaurants have been added' do
+  before do
+    Restaurant.create(name: 'KFC')
+  end
+
+  scenario 'display restaurants' do
+    visit '/restaurants'
+    expect(page).to have_content('KFC')
+    expect(page).not_to have_content('No restaurants yet')
+  end
+end
