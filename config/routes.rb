88fda34dd9b7c030cadf3ae39523ee2fd6ae7a75
Bernard Malhame <bernard.malhame@gmail.com>
2017-04-06 11:06:01 +0100

    added devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+root "restaurants#index"
   resources :restaurants do
     resources :reviews
   end
