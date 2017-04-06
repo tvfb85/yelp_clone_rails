bf3a60e093e23a6f0cff59681576db5d673b06d8
Vicky Roberts <vicki_uk@hotmail.com>
2017-04-06 09:47:07 +0100

    Cloning repo - V1,12

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews
+  end
 
   # Example resource route with options:
   #   resources :products do
