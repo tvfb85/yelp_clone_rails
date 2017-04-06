ac920b2961b28910d13f026abf74185cbbef5440
Bernard Malhame <bernard.malhame@gmail.com>
2017-04-06 12:43:33 +0100

    added facebook ominauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,8 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+  config.omniauth :facebook, "1888386381373014", "FACEBOOK_APP_SECRET",
+                  callback_url: "http://localhost:3000/users/auth/facebook/callback"
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
