18e04e4537d1d2959ec2b9f992168f55582ecb21
Bernard Malhame <bernard.malhame@gmail.com>
2017-04-06 10:38:53 +0100

    added error message for short name

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,8 +12,12 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render 'new'
+    end
   end
 
   def show
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -31,6 +31,17 @@ feature 'restaurants' do
         expect(current_path).to eq '/restaurants'
       end
     end
+
+    context 'an invalid restaurant' do
+        scenario 'does not let you submit a name that is too short' do
+          visit '/restaurants'
+          click_link 'Add a restaurant'
+          fill_in 'Name', with: 'kf'
+          click_button 'Create Restaurant'
+          expect(page).not_to have_css 'h2', text: 'kf'
+          expect(page).to have_content 'error'
+        end
+      end
   end
   context "viewing restaurants" do
 
